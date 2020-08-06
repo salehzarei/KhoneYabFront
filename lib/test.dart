19a9d69c4284983ca00e05d017f2a8cbcd2fd1cf
Saleh Zarei <saleh.zarei@gmail.com>
2020-08-06 11:24:49 +0430

    ok

---
+++ lib/test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Object>(
+      stream: null,
+      builder: (context, snapshot) {
+        return Container(
+          
+        );
+      }
+    );
+  }
+}
